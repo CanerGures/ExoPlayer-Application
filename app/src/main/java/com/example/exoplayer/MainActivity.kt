5109783f42f065cfc5c8d81ca86f918faa9f1a26
YusufCanerGures <gurescaner@gmail.com>
2020-11-03 17:18:45 +0300

    Live content and Vod are working fine.

--- app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.exoplayer
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -10,36 +11,32 @@ import com.ddd.androidutils.DoubleClick
 import com.ddd.androidutils.DoubleClickListener
 import com.example.exoplayer.api.model.GetJobsList
 import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.ui.TimeBar
 import com.google.android.exoplayer2.util.MimeTypes
 import com.google.android.exoplayer2.util.Util
-import java.util.concurrent.TimeUnit
 
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var playbackStateListener: Player.EventListener
+class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     private lateinit var playButton: ImageView
     private lateinit var forwardButton: ImageView
     private lateinit var backwardButton: ImageView
     private lateinit var closeButton: ImageView
     private lateinit var doubleClickForward: FrameLayout
     private lateinit var doubleClickBackward: FrameLayout
-    private lateinit var liveTimeBar: LinearLayout
     private lateinit var liveIcon: ImageView
-    private lateinit var timeBarBottom: TimeBar
     private lateinit var timeBarDuration: TextView
+    private lateinit var customseekbar: SeekBar
+    lateinit var runnable: Runnable
+
+    lateinit var handler: Handler
     private var playerView: PlayerView? = null
     private var player: SimpleExoPlayer? = null
     lateinit var currentJobObject: GetJobsList
     private var currentXmlLink: Any? = null
-
-    //lateinit var timeBar : LinearLayout
     private var playWhenReady = true
     private var currentWindow = 0
+    private var playonce = 0
     private var playbackPosition: Long = 0
     lateinit var playerPosition: TextView
 
@@ -52,12 +49,12 @@ class MainActivity : AppCompatActivity() {
         backwardButton = findViewById(R.id.centerBackwardButton)
         doubleClickForward = findViewById(R.id.doubleClickForward)
         doubleClickBackward = findViewById(R.id.doubleClickBackward)
-        playerPosition = findViewById(R.id.exo_position)
+        playerPosition = findViewById(R.id.exo_positionn)
         closeButton = findViewById(R.id.closeButton)
-        //timeBar = findViewById(R.id.timebarLayout)
-        timeBarBottom = findViewById(R.id.exo_progress)
-        timeBarDuration = findViewById(R.id.exo_duration)
+        customseekbar = findViewById(R.id.customseekbar)
+        timeBarDuration = findViewById(R.id.exo_durationn)
         liveIcon = findViewById(R.id.liveIcon)
+
 
         supportActionBar?.hide()
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
@@ -66,6 +63,8 @@ class MainActivity : AppCompatActivity() {
         if (temp != null) {
             currentJobObject = temp
         }
+
+
 
         closeButton.setOnClickListener {
             this@MainActivity.finish()
@@ -77,11 +76,19 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         if (Util.SDK_INT > 23) {
             currentXmlLink = intent.extras?.get("currentLink")
-            if (currentXmlLink != null) {
-                initializePlayerXml(currentXmlLink as String)
-            } else {
-                initializePlayer()
+            when (currentXmlLink) {
+                null -> {
+                    initializePlayer()
+                    setHandler()
+                    initializeSeekBar()
+                }
+                else -> {
+                    initializePlayerXml(currentXmlLink as String)
+                    setHandler()
+                    initializeSeekBar()
+                }
             }
+            Log.e("LIVEStart", player!!.isCurrentWindowLive.toString())
         }
     }
 
@@ -89,10 +96,18 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         if (Util.SDK_INT <= 23 || player == null) {
-            if (currentXmlLink != null) {
-                initializePlayerXml(currentXmlLink as String)
-            } else {
-                initializePlayer()
+            currentXmlLink = intent.extras?.get("currentLink")
+            when (currentXmlLink) {
+                null -> {
+                    initializePlayer()
+                    setHandler()
+                    initializeSeekBar()
+                }
+                else -> {
+                    initializePlayerXml(currentXmlLink as String)
+                    setHandler()
+                    initializeSeekBar()
+                }
             }
 
         }
@@ -123,6 +138,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun initializeSeekBar() {
+        customseekbar.progress = 0
+        customseekbar.setOnSeekBarChangeListener(this)
+
+        Log.e("LIVE", player!!.isCurrentWindowLive.toString())
+    }
 
 
     private fun initializePlayerXml(currentXmlLink: String) {
@@ -206,74 +227,21 @@ class MainActivity : AppCompatActivity() {
         player!!.addListener(object : Player.EventListener {
             override fun onTimelineChanged(timeline: Timeline, reason: Int) {
                 if (player!!.isCurrentWindowDynamic && player!!.isCurrentWindowLive) {
+                    customseekbar.progress = player!!.duration.toInt()
                     liveIcon.visibility = View.VISIBLE
-                    //timeBar.visibility = View.GONE
-                    liveTimeBar.visibility = View.VISIBLE
-
 
                 }
                 Log.e("timeline", "live:" + player!!.isCurrentWindowLive)
             }
 
-            override fun onExperimentalOffloadSchedulingEnabledChanged(offloadSchedulingEnabled: Boolean) {
-                Log.e("onexperimenta off load", "$offloadSchedulingEnabled")
-            }
-
-            override fun onIsLoadingChanged(isLoading: Boolean) {
-                Log.e("onIsLoadingChanged", "$isLoading")
-            }
-
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                Log.e("onIsPlayingChanged", "$isPlaying")
-            }
-
-            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
-                Log.e("onMediaItemTransition", "mediaItem:$mediaItem" + "   reason" + reason)
-            }
-
-            override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
-                Log.e(
-                    "onPlayWhenReadyChanged",
-                    "playwhenready:$playWhenReady" + "   reason" + reason
-                )
-            }
-
-            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
-                Log.e("PlaybkParametersChanged", "playbackParameters:$playbackParameters")
-            }
-
-            override fun onPlaybackStateChanged(state: Int) {
-                Log.e("onPlaybackStateChanged", "onPlaybackStateChanged:$state")
-            }
-
-            override fun onPlaybackSuppressionReasonChanged(playbackSuppressionReason: Int) {
-                Log.e("supressingreason", "supressingreason:$playbackSuppressionReason")
-            }
-
             override fun onPlayerError(error: ExoPlaybackException) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Error Occured! Player is closing.",
+                    Toast.LENGTH_SHORT
+                ).show()
                 Log.e("onPlayerError", "onPlayerError:$error")
-            }
-
-            override fun onPositionDiscontinuity(reason: Int) {
-                Log.e("onPositionDiscontinuity", "onPositionDiscontinuity:$reason")
-            }
-
-            override fun onRepeatModeChanged(repeatMode: Int) {
-                Log.e("onRepeatModeChanged", "onRepeatModeChanged:$repeatMode")
-            }
-
-            override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
-                Log.e("ShuffleEnabledChanged", "ShuffleEnabledChanged:$shuffleModeEnabled")
-            }
-
-            override fun onTracksChanged(
-                trackGroups: TrackGroupArray,
-                trackSelections: TrackSelectionArray
-            ) {
-                Log.e(
-                    "onTracksChanged",
-                    "trackGroups:$trackGroups" + "trackSelections$trackSelections"
-                )
+                finish()
             }
 
         })
@@ -343,7 +311,7 @@ class MainActivity : AppCompatActivity() {
         val link = "https://" + currentJobObject.streamLink
         val mediaItem =
             MediaItem.Builder()
-                .setUri("http://livesim.dashif.org/livesim/segtimeline_1/testpic_2s/Manifest.mpd")
+                .setUri("https://livesim.dashif.org/livesim/segtimeline_1/testpic_2s/Manifest.mpd")
                 .setMimeType(MimeTypes.APPLICATION_MPD)
                 .build()
         player!!.addMediaItem(mediaItem)
@@ -355,105 +323,11 @@ class MainActivity : AppCompatActivity() {
             override fun onTimelineChanged(timeline: Timeline, reason: Int) {
                 if (player!!.isCurrentWindowDynamic && player!!.isCurrentWindowLive) {
                     liveIcon.visibility = View.VISIBLE
-                    timeBarDuration.id = R.id.exo_position
 
 
                 }
-                Log.e("timeline", "live:" + player!!.isCurrentWindowLive)
-                Log.e("timeline", "duration:" + player!!.duration)
-                Log.e("timeline", "contentduration:" + player!!.contentDuration)
-                Log.e("timeline", "renderer count:" + player!!.rendererCount)
-                Log.e("timeline", "contentPosition:" + player!!.contentPosition)
-                Log.e("timeline", "currentPosition:" + player!!.currentPosition)
-                Log.e("timeline", "bufferedPosition:" + player!!.bufferedPosition)
-                Log.e("timeline", "totalBufferedDuration:" + player!!.totalBufferedDuration)
-                Log.e("timeline", "timeBarPosition:" + playerPosition.text)
-                Log.e("timeline", "timeBarDuration:" + timeBarDuration.text)
-                Log.e("timeline", "timeline:$timeline")
-
-                val duration = player!!.duration
-                val position = player!!.currentPosition
-
-                val hours = String.format("%02d", TimeUnit.MILLISECONDS.toHours(duration))
-                val minutes = String.format(
-                    "%02d",
-                    TimeUnit.MILLISECONDS.toMinutes(duration) - TimeUnit.HOURS.toMinutes(
-                        TimeUnit.MILLISECONDS.toHours(
-                            duration
-                        )
-                    )
-                )
-                val seconds = String.format(
-                    "%02d",
-                    TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(
-                        TimeUnit.MILLISECONDS.toMinutes(
-                            duration
-                        )
-                    )
-                )
-                val hoursPosition = String.format("%02d", TimeUnit.MILLISECONDS.toHours(duration))
-                val minutesPostion = String.format(
-                    "%02d",
-                    TimeUnit.MILLISECONDS.toMinutes(position) - TimeUnit.HOURS.toMinutes(
-                        TimeUnit.MILLISECONDS.toHours(
-                            position
-                        )
-                    )
-                )
-                val secondsPosition = String.format(
-                    "%02d",
-                    TimeUnit.MILLISECONDS.toSeconds(position) - TimeUnit.MINUTES.toSeconds(
-                        TimeUnit.MILLISECONDS.toMinutes(
-                            position
-                        )
-                    )
-                )
-                if (hours == "00") {
-                    val timeFormat = "-$minutes.$seconds"
-                    Log.e("timeline", "timeFormat:$timeFormat")
-                    timeBarDuration.text = timeFormat
-                } else {
-                    val timeFormat = "-$hours.$minutes.$seconds"
-                    Log.e("timeline", "timeFormat:$timeFormat")
-                    timeBarDuration.text = timeFormat
-                }
 
 
-            }
-
-            override fun onExperimentalOffloadSchedulingEnabledChanged(offloadSchedulingEnabled: Boolean) {
-                Log.e("Experimental off load", "$offloadSchedulingEnabled")
-            }
-
-            override fun onIsLoadingChanged(isLoading: Boolean) {
-                Log.e("onIsLoadingChanged", "$isLoading")
-            }
-
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                Log.e("onIsPlayingChanged", "$isPlaying")
-            }
-
-            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
-                Log.e("onMediaItemTransition", "mediaItem:$mediaItem" + "   reason" + reason)
-            }
-
-            override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
-                Log.e(
-                    "onPlayWhenReadyChanged",
-                    "playwhenready:$playWhenReady" + "   reason" + reason
-                )
-            }
-
-            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
-                Log.e("PlaybkParametersChanged", "playbackParameters:$playbackParameters")
-            }
-
-            override fun onPlaybackStateChanged(state: Int) {
-                Log.e("onPlaybackStateChanged", "onPlaybackStateChanged:$state")
-            }
-
-            override fun onPlaybackSuppressionReasonChanged(playbackSuppressionReason: Int) {
-                Log.e("supressingreason", "supressingreason:$playbackSuppressionReason")
             }
 
             override fun onPlayerError(error: ExoPlaybackException) {
@@ -466,47 +340,108 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
 
-            override fun onPositionDiscontinuity(reason: Int) {
-                Log.e("onPositionDiscontinuity", "onPositionDiscontinuity:$reason")
-            }
-
-            override fun onRepeatModeChanged(repeatMode: Int) {
-                Log.e("onRepeatModeChanged", "onRepeatModeChanged:$repeatMode")
-            }
-
-            override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
-                Log.e("ShuffleEnabledChanged", "ShuffleEnabledChanged:$shuffleModeEnabled")
-            }
-
-            override fun onTracksChanged(
-                trackGroups: TrackGroupArray,
-                trackSelections: TrackSelectionArray
-            ) {
-                Log.e(
-                    "onTracksChanged",
-                    "trackGroups:$trackGroups" + "trackSelections$trackSelections"
-                )
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                if (playonce == 0 && player!!.isCurrentWindowLive) {
+                    player!!.seekTo(player!!.duration)
+                    playonce += 1
+                }
             }
 
 
         })
+
         player!!.prepare()
+        Log.e("LIVEOncreate", player!!.isCurrentWindowLive.toString())
+    }
+
+    private fun setHandler() {
+        Log.e("LIVEHandlerTop", player!!.isCurrentWindowLive.toString())
+        handler = Handler()
+
+        runnable = Runnable {
+            customseekbar.max = player!!.duration.toInt()
+            if (player!!.isCurrentWindowDynamic && player!!.isCurrentWindowLive) {
+                if (player!!.duration > 0) {
+                    val currentduration = player!!.currentPosition
+                    customseekbar.progress = currentduration.toInt()
+                    timeBarDuration.text = "-" + convertTime(player!!.duration)
+                    playerPosition.text = "-" + convertTime(player!!.duration - currentduration)
+                    Log.e("current", "$currentduration")
+
+                }
+            } else {
+
+                if (player!!.duration > 0) {
+                    val currentduration = player!!.currentPosition
+                    customseekbar.progress = currentduration.toInt()
+                    timeBarDuration.text = "" + convertTime(currentduration)
+                    playerPosition.text = "" + convertTime(player!!.duration - currentduration)
+                }
+            }
+            handler.postDelayed(runnable, 500)
+
+        }
+        handler.post(runnable)
+        Log.e("LIVEHandlerBot", player!!.isCurrentWindowLive.toString())
+
+    }
+
+
+    private fun convertTime(ms: Long): String {
+        var time: String = ""
+        var x: Int = (ms / 1000).toInt()
+        val seconds = x % 60
+        x /= 60
+        val minutes = x % 60
+        x /= 60
+        val hours = x % 24
+        time = if (hours != 0) {
+            String.format("%02d", hours) + ":" + String.format(
+                "%02d",
+                minutes
+            ) + ":" + String.format(
+                "%02d",
+                seconds
+            )
+        } else {
+            String.format("%02d", minutes) + ":" + String.format("%02d", seconds)
+        }
+        return time
     }
 
     private fun releasePlayer() {
         if (player != null) {
             player!!.stop()
+            handler.removeCallbacks(runnable)
             playerView!!.player = null
             player!!.release()
         }
     }
 
-    fun convertSecondsToHMmSs(seconds: Long): String? {
-        val s = seconds % 60
-        val m = seconds / 60 % 60
-        val h = seconds / (60 * 60) % 24
-        return String.format("%d:%02d:%02d", h, m, s)
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        when (seekBar!!.id) {
+            R.id.customseekbar -> {
+                if (fromUser && player!!.isCurrentWindowDynamic && player!!.isCurrentWindowLive) {
+                    player!!.seekTo(progress.toLong())
+                    val currentduration = player!!.currentPosition
+                    timeBarDuration.text = "-" + convertTime(player!!.duration)
+                    playerPosition.text = "-" + convertTime(player!!.duration - currentduration)
+                } else if (fromUser) {
+                    player!!.seekTo(progress.toLong())
+                    val currentduration = player!!.currentPosition
+                    timeBarDuration.text = "" + convertTime(currentduration)
+                    playerPosition.text = "" + convertTime(player!!.duration - currentduration)
+                }
+            }
+        }
     }
 
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+    }
 }
 
